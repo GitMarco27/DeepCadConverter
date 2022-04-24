d01e9aead454b40bab4549ba7c19705d76c3e9e3
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-04-24 14:21:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import trimesh
+
+
+@st.cache
+def convert_cad(geometry):
+    mesh = trimesh.Trimesh(**trimesh.interfaces.gmsh.load_gmsh(file_name=geometry, gmsh_args=[
+        ("Mesh.Algorithm", 1),  # Different algorithm types, check them out
+        ("Mesh.CharacteristicLengthFromCurvature", 50),  # Tuning the smoothness, + smothness = + time
+        ("General.NumThreads", 10),  # Multithreading capability
+        ("Mesh.MinimumCirclePoints", 32)]))
+    return mesh
+
+# Add sidebar to the app
+st.sidebar.markdown("### My first Awesome App")
+st.sidebar.markdown("Welcome to my first awesome app. This app is built using Streamlit and uses data source from redfin housing market data. I hope you enjoy!")
+
+# Add title and subtitle to the main interface of the app
+st.title("CAD to STL converter")
+st.markdown('### By Marco Sanguineti')
+
+data = st.file_uploader("Upload your cad files", type={"iges", "igs"})
+new_data = convert_cad(data)
+st.write(data)
