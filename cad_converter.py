f321d5ed8eef581f7b7b0281c0a550bde66e86f2
GitMarco27 <72693100+GitMarco27@users.noreply.github.com>
2022-04-25 11:10:20 +0200

    Formatting updates

--- cad_converter.py
+++ cad_converter.py
@@ -3,12 +3,28 @@ import trimesh
 
 
 def convert_cad(source, dest):
+    # General info found on https://trimsh.org/trimesh.interfaces.gmsh.html Returns a surface mesh from CAD model in
+    # Open Cascade Breap (.brep), Step (.stp or .step) and Iges formats Or returns a surface mesh from 3D volume mesh
+    # using gmsh. For a list of possible options to pass to GMSH, check: http://gmsh.info/doc/texinfo/gmsh.html
     mesh = trimesh.Trimesh(**trimesh.interfaces.gmsh.load_gmsh(file_name=source, gmsh_args=[
         ("Mesh.Algorithm", 1),  # Different algorithm types, check them out
         ("Mesh.CharacteristicLengthFromCurvature", 50),  # Tuning the smoothness, + smothness = + time
         ("General.NumThreads", 10),  # Multithreading capability
         ("Mesh.MinimumCirclePoints", 32)]))
+
+    # scene = mesh.scene()
+
+    # window_conf = gl.Config(double_buffer=True, depth_size=6)
+    # png = scene.save_image(resolution=[1920, 1080],
+    #                        window_conf=window_conf,
+    #                        visible=False)
+
+    # rendered = Image.open(trimesh.util.wrap_as_stream(png))
+    # rendered.save('tmp.png')
+
     mesh.export(dest)
+
+    # png = PIL.Image.open(scene.save_image(resolution=(1920, 1080)))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,34 +5,53 @@ import streamlit as st
 
 # Add sidebar to the app
 st.sidebar.markdown("### Cad to Stl Converter")
-st.sidebar.markdown("Welcome to my first awesome app. This app is built using Streamlit and converts cad files into an stl format. I hope you enjoy!")
+st.sidebar.markdown("This app is built using Streamlit and converts Cad files into an **Stl** format.")
+st.sidebar.markdown("""
+**Supported formats**:
+
+    - .step / .stp
+    - .iges / .igs
+
+### Credits
+**Marco Sanguineti**
+
+* marco.sanguineti.info@gmail.com
+* [LinkedIn](https://www.linkedin.com/in/marco-sanguineti-088604161/)
+* [Medium](https://medium.com/@marcosanguineti) 
+
+""")
 
 # Add title and subtitle to the main interface of the app
 st.title("CAD to STL converter")
-st.markdown('### By Marco Sanguineti - GitMarco27')
+st.markdown('*By Marco Sanguineti - GitMarco27*')
+st.markdown('---')
 
-data = st.file_uploader("Upload your cad files", type={"iges", "igs"}, accept_multiple_files=True)
+# Loading data
+data = st.file_uploader("Upload your cad files", type={"iges", "igs", "step", "stp"}, accept_multiple_files=True)
 
-if not os.path.exists('tempDir'):
-    os.mkdir('tempDir')
-for file in os.listdir('tempDir'):
-    os.remove(os.path.join('tempDir', file))
-if os.path.exists('archive.zip'):
-    os.remove('archive.zip')
+# Converting files if requested
+if st.button('Convert your files'):
+    # Clearing files, if exist
+    if not os.path.exists('tempDir'):
+        os.mkdir('tempDir')
+    for file in os.listdir('tempDir'):
+        os.remove(os.path.join('tempDir', file))
+    if os.path.exists('archive.zip'):
+        os.remove('archive.zip')
 
-if len(data) > 0:
-    for cad in data:
-        source = os.path.join("tempDir", cad.name)
-        dest = os.path.join("tempDir", os.path.splitext(cad.name)[0]+'.stl')
-        with open(os.path.join("tempDir", cad.name), "wb") as f:
-            f.write(cad.getbuffer())
+    if len(data) > 0:
+        for cad in data:
+            source = os.path.join("tempDir", cad.name)
+            dest = os.path.join("tempDir", os.path.splitext(cad.name)[0]+'.stl')
+            with open(os.path.join("tempDir", cad.name), "wb") as f:
+                f.write(cad.getbuffer())
 
-        os.system(f'{sys.executable} cad_converter.py {source} {dest}')
+            os.system(f'{sys.executable} cad_converter.py {source} {dest}')
 
-    st.markdown('Your .stl files are ready!')
+        st.markdown('Your .stl files are ready!')
 
-    shutil.make_archive('archive', 'zip', 'tempDir')
+        shutil.make_archive('archive', 'zip', 'tempDir')
 
-    with open('archive.zip', 'rb') as f:
-        st.download_button('Download', f, file_name='archive.zip')
+        with open('archive.zip', 'rb') as f:
+            st.download_button('Download', f, file_name='archive.zip')
 
